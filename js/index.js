ccce85afe3f1d64fe97a02c51808cd15521cd00b
enbatminton <enbatminton@gmail.com>
2024-06-17 21:36:36 +0900

    Update index.js
    minify

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-$(function () {
-    var pagetop = $('#page-top');
-    pagetop.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            pagetop.fadeIn();
-        } else {
-            pagetop.fadeOut();
-        }
-    });
-    pagetop.click(function () {
-        $('body, html').animate({ scrollTop: 0 }, 500);
-        return false;
-    });
-});
+$(function(){var o=$("#page-top");o.hide(),$(window).scroll(function(){$(this).scrollTop()>100?o.fadeIn():o.fadeOut()}),o.click(function(){return $("body, html").animate({scrollTop:0},500),!1})});
